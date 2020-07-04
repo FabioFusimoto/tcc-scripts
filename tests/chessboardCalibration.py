1eb8537657c3dc306089dc6021ee69bc149d742f
FabioFusimoto <fabiofusimoto.pires@hotmail.com>
2020-07-04 19:53:28 -0300

    Draw axis and coordinates over chessboard image

--- tests/chessboardCalibration.py
+++ tests/chessboardCalibration.py
@@ -2,10 +2,10 @@ import pprint
 
 import src.calibration.chessboard as chess
 
-def getCoefficients(imgSrcPath, imgOutPath, imgPrefix, imgFormat, sqrSize, boardWidth, boardHeight, downsize, scale, calibrationFilename):
+def getCoefficients(imgSrcPath, imgOutPath, imgPrefix, imgFormat, sqrSize, boardWidth, boardHeight, scale, calibrationFilename):
     '''Calculates the calibration coefficients, prints them and stores them as a YML file'''
     ret, mtx, dist, rvecs, tvecs = chess.calibrate(imgSrcPath, imgOutPath, imgPrefix, imgFormat, sqrSize,
-                                                   boardWidth, boardHeight, downsize, scale)
+                                                   boardWidth, boardHeight, scale)
 
     print('\nReturn value:')
     pprint.pprint(ret)
@@ -32,39 +32,27 @@ def undistortImage(imageSource, imageOutput, calibrationFile, scale):
     cameraMatrix, distortionCoefficients = chess.loadCalibrationCoeficients(calibrationFile)
     chess.undistortImage(imageSource, imageOutput, cameraMatrix, distortionCoefficients, scale)
 
-def drawAxisOnChessboardImage(imageSource, imageOutput, axisLength, calibrationFile, width=9, height=6, scale=0.75):
-    '''Draws an axis on a chessboard figure and optionally saves it to imageOutput'''
-    
-    cameraMatrix, distortionCoefficients = chess.loadCalibrationCoeficients(calibrationFile)
-    chess.drawOnChessboard(imageSource, imageOutput, axisLength, cameraMatrix, distortionCoefficients,
-                           width, height, scale)
-
-def findChessboardCoordinates(imageSource, calibrationFile, squareSize, width, height, scale):
-    cameraMatrix, distortionCoefficients = chess.loadCalibrationCoeficients(calibrationFile)
-    rotationVectors, translationVectors = chess.getPositionVectors(imageSource, cameraMatrix, distortionCoefficients,
-                                                                   squareSize, width, height, scale)
-    
-    print('\nRotation vectors:')
-    pprint.pprint(rotationVectors)
-
-    print('\nTranslation vectors:')
-    pprint.pprint(translationVectors)
-
 def drawPositionVectors(imageSource, imageOutput, calibrationFile, squareSize, width, height, scale):
     cameraMatrix, distortionCoefficients = chess.loadCalibrationCoeficients(calibrationFile)
     chess.drawPositionVectors(imageSource, imageOutput, cameraMatrix, distortionCoefficients, squareSize, width, height, scale)
 
-# getCoefficients('images/for-calibration', 'images/calibration-output', 'X', 'jpg', 0.228/9, 9, 6, True, 0.75,
+# getCoefficients('images/for-calibration', 'images/calibration-output', 'X', 'jpg', 0.228/9, 9, 6, 0.75,
 #                 'tests/calibration-coefficients/g7-play-X-75-percent-resolution.yml') # measured 22.8cm - 9 squares
 
 # undistortImage('images/for-calibration/C11.jpg', 'images/calibration-output/C11-undistorted.jpg', 
 #                'tests/calibration-coefficients/g7-play-75-percent-resolution.yml', 0.75)
 
-# drawAxisOnChessboardImage('images/for-calibration/C11.jpg', 'images/calibration-output/C11-axis.jpg', 2,
-#                           'tests/calibration-coefficients/g7-play-75-percent-resolution.yml', 9, 6 , 0.75)
-
 # findChessboardCoordinates('images/for-calibration/D100cm.jpg', 'tests/calibration-coefficients/g7-play-100-percent-resolution.yml',
 #                           0.228/9, 9, 6, 1.0) # measured 22.8cm - 9 squares
 
-drawPositionVectors('images/for-calibration/Z100-00.jpg', 'images/calibration-output/Z100-00-coords.jpg',
-                    'tests/calibration-coefficients/g7-play-X-75-percent-resolution.yml', 0.228/9, 9, 6, 0.75)
+poseCalibImages = ['Z50-00', 'Z50-01', 'Z50-02','Z75-00','Z75-01','Z75-02','Z100-00','Z100-01','Z100-02']
+calibrationFile = 'tests/calibration-coefficients/g7-play-X-75-percent-resolution.yml'
+
+for img in poseCalibImages:
+    print('\nEstimating pose on ' + img)
+    inputFolder = 'images/for-calibration/'
+    outputFolder = 'images/calibration-output/'
+    extension = '.jpg'
+    outputFile = outputFolder + img + '-coords' + extension
+    drawPositionVectors(inputFolder + img + extension, outputFile, 
+                        calibrationFile, 0.228/9, 9, 6, 0.75)
