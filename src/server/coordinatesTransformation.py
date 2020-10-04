78a6bf697b5ac010c4d9713c1c433d302aff533c
FabioFusimoto <fabiofusimoto.pires@hotmail.com>
2020-10-04 17:28:29 -0300

    transformation adjustments

--- src/server/coordinatesTransformation.py
+++ src/server/coordinatesTransformation.py
@@ -40,9 +40,9 @@ def vidaEContextTransformations(pose, objectType):
         'hmd': {'0_roll':  +math.degrees(pose['yaw']),
                 '1_pitch': +math.degrees(pose['roll']) + 180,
                 '2_yaw':   +math.degrees(pose['pitch']),
-                '3_x':     +906 - pose['z'],
+                '3_x':     +900 - pose['z'],
                 '4_y':      -13 + pose['x'],
-                '5_z':     +150 + pose['y']}
+                '5_z':     +120 + pose['y']}
     }.get(objectType, {'0_roll':  +math.degrees(pose['roll']),
                        '1_pitch': +math.degrees(pose['pitch']),
                        '2_yaw':   +math.degrees(pose['yaw']),
